0dafab69cb8f5c42f744320434c8d9e27dbe8640
Josh Hapner <joshua_hapner1@taylor.edu>
2012-11-18 20:06:05 -0500

    advertisement complete except for board width and height comparison validation

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,8 +1,15 @@
 class Advertisement < ActiveRecord::Base
-  attr_accessible :board_id, :height, :image, :user_id, :width, :x_location, :y_location
+  attr_accessible :height, :image, :width, :x_location, :y_location
+  attr_protected :board_id, :user_id
 
   has_many :tiles
   has_many :payment_details, as: :payable
   belongs_to :user
   belongs_to :board
+
+  validates :x_location, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :y_location, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :width, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :height, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :image, presence: true
 end
