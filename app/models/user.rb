673fd46b9c4e9b43c68299667ec5ca334995252c
Josh Hapner <joshua_hapner1@taylor.edu>
2012-11-18 21:55:07 -0500

    models mostly working except for 6 tests in ads and 6 tests in tiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :admin, :email, :name, :password_digest, :remember_token
-
+  attr_accessible :email, :name, :password_digest, :remember_token
+  attr_protected :admin
+  
   has_many :boards
   has_many :advertisements
   has_many :payment_details
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
